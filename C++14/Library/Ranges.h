58bbbd51516dc2b9d3dc730f4a9c8a3bbf2046c9
Nicholas Timmons <ngtimmons@gmail.com>
2017-10-31 16:36:38 +0000

    Updated filesystem and reverseiterators

--- C++14/Library/Ranges.h
+++ C++14/Library/Ranges.h
@@ -2,12 +2,35 @@
 
 //#ifdef MSVC_NOT_SUPPORTED_ENABLED
 
-namespace Ranges_Example
+#include <vector>
+#include <iostream>
+#include <iterator>
+
+namespace ReverseIterators_Example
 {
-	//TODO http://en.cppreference.com/w/cpp/iterator/rbegin
-	void RangesIterator()
+	void ReverseIterators()
 	{
+		//Simple reverse iterator on a vector
+		std::vector<int> a = { 5,6,7 };
+		std::reverse_iterator<std::_Vector_iterator<std::_Vector_val<std::_Simple_types<int> > > > ai = std::rbegin(a);
+		std::cout << *ai << '\n';
+
+		// More readable as:
+		std::vector<int> b = { 5,6,7 };
+		auto bi = std::rbegin(b);
+		std::cout << *bi << '\n';
+
+		// Const reverse iterator iterator
+		std::vector<int> c = { 5,6,7 };
+		std::reverse_iterator<std::_Vector_const_iterator<std::_Vector_val<std::_Simple_types<int> > > > ci = std::crbegin(c);
+		std::cout << *ci << '\n';
+
+		// More readable:
+		std::vector<int> d = { 5,6,7 };
+		auto di = std::crbegin(d);
+		std::cout << *di << '\n';
 	}
+	
 
 }
 
--- C++17/Library/Filesystem.h
+++ C++17/Library/Filesystem.h
@@ -1,9 +1,10 @@
 #pragma once
 
-//#ifdef MSVC_NOT_SUPPORTED_ENABLED
+
 
 #include <filesystem>
 #include <iostream>
+
 namespace Filesystem_Example
 {
 	void Filesystem()
@@ -23,6 +24,143 @@ namespace Filesystem_Example
 					<< "filename() = "		<< pathToShow.filename()		<< "\n"
 					<< "stem() = "			<< pathToShow.stem()			<< "\n"
 					<< "extension() = "		<< pathToShow.extension()		<< "\n";
+
+		//std::path
+		std::experimental::filesystem::path someFilePath("C:\\Windows\\notepad.exe");
+		
+		//std::directory_entry
+		std::experimental::filesystem::directory_entry someDirectory("C:\\Windows\\");
+
+		//std::directory_iterator
+		std::experimental::filesystem::directory_iterator dir_iter(someDirectory);
+		for (auto& p : dir_iter)
+			std::cout << p << '\n';
+		
+		//std::recursive_directory_iterator
+		std::experimental::filesystem::recursive_directory_iterator dir_rev_iter(someDirectory);
+		for (auto& p : dir_rev_iter)
+			std::cout << p << '\n';
+
+		//std::file_status
+		std::experimental::filesystem::file_status file_status_obj = std::experimental::filesystem::status(someFilePath);
+
+		//std::space_info
+		std::experimental::filesystem::space_info cSpace = std::experimental::filesystem::space("C:\\");
+
+		std::cout	<< ".        Capacity       Free      Available\n"
+					<< "C:\\:   "	<<	cSpace.capacity << "   "
+					<< cSpace.free	<< "   "			<< cSpace.available << '\n';
+
+		//std::file_type 
+
+		//std::file_time_type
+		std::experimental::filesystem::file_time_type ftime = std::experimental::filesystem::last_write_time(someFilePath);
+		//std::perms
+		std::experimental::filesystem::perms permissions = file_status_obj.permissions();
+
+		//std::perm_options
+		#ifdef MSVC_NOT_SUPPORTED_ENABLED
+
+		std::experimental::filesystem::permissions(somePath, std::experimental::filesystem::perms::owner_all | 
+															 std::experimental::filesystem::perms::group_all, 
+															 std::experimental::filesystem::perm_options::add);
+		#endif
+		
+		//std::directory_options
+			//Not used or fully defined in MSVC
+			//std::experimental::filesystem::directory_options::follow_directory_symlink
+
+		//std::file_time_type | std::last_write_time
+		std::experimental::filesystem::file_time_type fileTime = std::experimental::filesystem::last_write_time(someFilePath);
+
+		//std::absolute | std::equivalent
+		std::experimental::filesystem::path relPath(".\\CPPExtensionExample.exe");
+		std::experimental::filesystem::path absPath = std::experimental::filesystem::absolute(relPath);
+		bool isAbs = absPath.is_absolute();
+		bool isRelative = absPath.is_relative();
+		bool isPathSame = std::experimental::filesystem::equivalent(relPath, absPath);
+
+		//std::canonical | std::weakly_canonical
+		std::experimental::filesystem::path cannonicalPath = std::experimental::filesystem::canonical(relPath);
+
+		#ifdef MSVC_NOT_SUPPORTED_ENABLED
+		std::experimental::filesystem::path weaklyCannonicalPath = std::experimental::filesystem::weakly_canonical(relPath);
+		#endif
+		
+		//std::relative
+		#ifdef MSVC_NOT_SUPPORTED_ENABLED
+		std::experimental::filesystem::path relativePath = std::experimental::filesystem::relative(somePath);
+		#endif
+
+		//std::proximate
+		#ifdef MSVC_NOT_SUPPORTED_ENABLED
+		std::experimental::filesystem::path relativePath = std::experimental::filesystem::proximate(somePath);
+		#endif
+
+		//std::copy
+		std::experimental::filesystem::copy(someFilePath, ".\\notepad.exe", std::experimental::filesystem::copy_options::overwrite_existing);
+			//copy_options::recursive is also useful for but trick to demo
+
+		//std::copy_file
+		std::experimental::filesystem::copy_file(someFilePath, ".\\notepad.exe", std::experimental::filesystem::copy_options::overwrite_existing);
+
+		//std::copy_symlink
+		//TODO
+		//std::create_directory
+		std::experimental::filesystem::create_directory("New_Dir");
+
+		//std::create_directories
+		std::experimental::filesystem::create_directories("New_Dirs/a");
+
+		//std::create_hard_link
+		//TODO
+
+		//std::create_symlink
+		//TODO
+
+		//std::create_directory_symlink
+		//TODO
+
+		//std::current_path
+		std::experimental::filesystem::path thisLocation = std::experimental::filesystem::current_path();
+		std::cout << "Current directory = " << thisLocation.c_str() << "\n";
+		std::experimental::filesystem::current_path(someDirectory);
+		std::experimental::filesystem::path newLocLocation = std::experimental::filesystem::current_path();
+		std::cout << "Changed working directory to = " << newLocLocation.c_str() << "\n";
+		std::experimental::filesystem::current_path(thisLocation);
+		std::experimental::filesystem::path thisLocation = std::experimental::filesystem::current_path();
+		std::cout << "Reset working directory to = " << newLocLocation.c_str() << "\n";
+
+		//std::exists
+		bool doesFileExist = std::experimental::filesystem::exists(someFilePath);
+
+		//std::file_size
+		std::uintmax_t filesize = std::experimental::filesystem::file_size(someFilePath);
+
+		//std::hard_link_count
+		//TODO
+
+		//std::read_symlink
+		//TODO
+
+		//std::remove
+		//std::remove_all
+		//std::rename
+		//std::resize_file
+		//std::space
+		//std::status
+		//std::status_known
+		//std::symlink_status
+		//std::temp_directory_path
+		//std::is_block_file
+		//std::is_character_file
+		//std::is_directory
+		//std::is_empty
+		//std::is_fifo
+		//std::is_other
+		//std::is_regular_file
+		//std::is_socket
+		//std::is_symlink
 
 		//TODO
 		// - Directory
